e43cfe90c8d7ec4d53587373fbce161b7dd89d39
Alvin <samacoa@uci.edu>
2015-01-17 15:34:09 -0800

    added score prefabs and score script

---
+++ FronthandBackhand/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreSystem : MonoBehaviour {
+	public int currentScore;
+	public int highscore;
+	public int multiplier;
+	public GUIText scoreText;
+	public GUIText multiplierText;
+	
+	
+	// Use this for initialization
+	void Start () {
+		currentScore = 0;
+		highscore = 0;
+		multiplier = 0;
+		UpdateScore ();
+	}
+	
+	void resetMultiplier() {
+		multiplier = 0;
+	}
+	
+	void adjustScore( int score ) {
+		currentScore += score;
+		UpdateScore ();
+	}
+	
+	// Update is called once per frame
+	void UpdateScore () {
+		scoreText.text = "Score: " + currentScore;
+		multiplierText.text = "Multiplier: " + multiplier;
+	}
+}
