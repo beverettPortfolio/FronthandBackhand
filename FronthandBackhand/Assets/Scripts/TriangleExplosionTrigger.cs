8c9af84cf5dc320051d720ab8fa6eb0c9bb6f659
bombguy <cshin3@uci.edu>
2015-01-18 15:25:48 -0800

    Organized Files, Triangle explodes,

---
+++ FronthandBackhand/Assets/Scripts/TriangleExplosionTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriangleExplosionTrigger : MonoBehaviour {
+	public BallControllerScript ballcontrol;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ballcontrol.song.time == 0) {
+			anim.SetTrigger ("Explode");
+		}
+	}
+}
