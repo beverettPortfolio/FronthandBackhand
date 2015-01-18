9d1500e3b6b202044ea231b81ced57d7e1fa9aac
bombguy <cshin3@uci.edu>
2015-01-17 16:06:10 -0800

    score system, colliders
    updated prefabs

---
+++ FronthandBackhand/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreSystem : MonoBehaviour {
+	public int currentScore;
+	public int highscore;
+	public int multiplier;
+	public GUIText scoreText;
+	public GUIText multiplierText;
+
+
+	// Use this for initialization
+	void Start () {
+		currentScore = 0;
+		highscore = 0;
+		multiplier = 1;
+	}
+
+	public void resetMultiplier() {
+		multiplier = 1;
+	}
+
+	public void increaseMultiplier(int inc) {
+		multiplier+= inc;
+	}
+
+	public void adjustScore( int score ) {
+		currentScore += score;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "Score: " + currentScore;
+		multiplierText.text = "Multiplier: " + multiplier;
+	}
+}
