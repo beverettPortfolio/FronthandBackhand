0ea914f2ba657b51f856987e7a82229c2cc549f2
Cory Sherman <coryshrmn@gmail.com>
2015-01-18 11:31:42 -0800

    balls are eaten or rejected, speed up spin

---
+++ FronthandBackhand/Assets/BallAccepting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallAccepting : MonoBehaviour {
+
+	public float ShrinkDuration;
+
+	private float _createdTime;
+	private Vector3 _initialScale;
+
+	// Use this for initialization
+	void Start () {
+		_createdTime = Time.time;
+		_initialScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float elapsed = Time.time - _createdTime;
+		float phase = Mathf.Clamp01(elapsed / ShrinkDuration);
+		transform.localScale = Vector3.Lerp (_initialScale, Vector3.zero, phase);
+		if(phase >= 1.0f) {
+			Destroy (gameObject);
+		}
+	}
+}
