b72093863164de6f934fc23e9d7379e731a2109c
bombguy <cshin3@uci.edu>
2015-01-17 17:29:30 -0800

    Ball Movement

---
+++ FronthandBackhand/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void FixedUpdate () {
+		Vector3 position = transform.position;
+		position -= position.normalized * Speed;
+		transform.position = position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
