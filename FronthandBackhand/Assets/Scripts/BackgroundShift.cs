a9cf36dabb0ab28e316be59fc0d3a9d83ecbac94
Cory Sherman <coryshrmn@gmail.com>
2015-01-18 13:42:06 -0800

    separate musicsync logic

--- FronthandBackhand/Assets/Scripts/BackgroundShift.cs
+++ FronthandBackhand/Assets/Scripts/BackgroundShift.cs
@@ -3,57 +3,51 @@ using System.Collections;
 
 public class BackgroundShift : MonoBehaviour {
 
-	public float BPM;
-	public int TimeSignature;
-	public AudioSource Song;
-
+	public MusicSync Music;
 	
 	public float SaturationStart;
 	public float BrightnessStart;
 	public float SaturationEnd;
 	public float BrightnessEnd;
 
-	private float _lastBeat;
+	private int _lastIMeasure;
 	private float _lastHue;
 	private float _hue;
 	
 	// Use this for initialization
 	void Start () {
-		_lastBeat = -100.0f;
+		_lastIMeasure = -1;
 		_lastHue = 0.0f;
 		_hue = 0.0f;
+	}
+
+	private void ChangeHue() {
+			_lastHue = _hue;
+			_hue += Random.Range(60.0f, 300.0f);
+			_hue = Mathf.Repeat(_hue, 360.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float beat = (BPM / 60.0f) * ((float)Song.timeSamples / (float)Song.clip.frequency);
-		int iBeat = Mathf.FloorToInt (beat);
-		int iLastBeat = Mathf.FloorToInt (_lastBeat);
-
-		// if new measure
-		if (iBeat / TimeSignature != iLastBeat / TimeSignature) {
-			_lastHue = _hue;
-			_hue += Random.Range(60.0f, 300.0f);
-			_hue = Mathf.Repeat(_hue, 360.0f);
+		if(_lastIMeasure != Music.IMeasure) {
+			_lastIMeasure = Music.IMeasure;
+			ChangeHue ();
 		}
-		//float measurePhase = (beat - (iBeat / TimeSignature * TimeSignature)) / TimeSignature;
-		float beatPhase = beat - iBeat;
 
 		float h;
-		if (iBeat % TimeSignature == 0) {
-			h = Mathf.LerpAngle (_lastHue, _hue, beatPhase);
+		if (Music.IBeat % Music.TimeSignature == 0) {
+			h = Mathf.LerpAngle (_lastHue, _hue, Music.BeatPhase);
 		}
 		else {
 			h = _hue;
 		}
 
 		h = Mathf.Repeat (h, 360.0f) / 360.0f;
-		float s = Mathf.Lerp (SaturationStart, SaturationEnd, beatPhase);
-		float b = Mathf.Lerp (BrightnessStart, BrightnessEnd, beatPhase);
+		float s = Mathf.Lerp (SaturationStart, SaturationEnd, Music.BeatPhase);
+		float b = Mathf.Lerp (BrightnessStart, BrightnessEnd, Music.BeatPhase);
 		HSBColor hsbColor = new HSBColor (h, s, b);
 		Color color = HSBColor.ToColor (hsbColor);
 
 		Camera.main.backgroundColor = color;
-		_lastBeat = beat;
 	}
 }
---
+++ FronthandBackhand/Assets/Scripts/MusicSync.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSync : MonoBehaviour {
+
+	public float BPM;
+	public int TimeSignature;
+	public AudioSource Song;
+
+	public float Beat {
+		get { return (BPM / 60.0f) * ((float)Song.timeSamples / (float)Song.clip.frequency); }
+	}
+
+	public int IBeat {
+		get { return Mathf.FloorToInt (Beat); }
+	}
+
+	public float BeatPhase {
+		get { return Beat - IBeat; }
+	}
+
+	public float Measure {
+		get { return Beat / TimeSignature; }
+	}
+
+	public int IMeasure {
+		get { return Mathf.FloorToInt (Measure); }
+	}
+
+	public float MeasurePhase {
+		get { return Measure / TimeSignature; }
+	}
+}
