c05eb52084b64fee5f2ab409d69843c65cccc898
beverettPortfolio <ben.y.everett@gmail.com>
2016-09-14 02:41:23 -0700

    Added more documentation

--- FronthandBackhand/Assets/Scripts/BallControllerScript.cs
+++ FronthandBackhand/Assets/Scripts/BallControllerScript.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.IO;
 using System;
 
+/// <summary>
+/// A class that spawns the balls for the level based on a separate level data file.
+/// More levels can easily be added by creating more level text files.
+/// </summary>
 public class BallControllerScript : MonoBehaviour {
 	public int BPM;
 	public AudioSource song;
@@ -32,7 +36,7 @@ public class BallControllerScript : MonoBehaviour {
 		colorCodeTags = new String[]{"RedBall", "GreenBall", "BlueBall"};
 	}
 	
-	// Update is called once per physics-frame
+	// FixedUpdate is called once per physics-frame
 	void FixedUpdate () {
 		if (leftIndex < beatMapLeft.Count) {		
 			double leftTime = beatMapLeft[leftIndex][0]/(BPM/60)*song.clip.frequency;
@@ -50,6 +54,14 @@ public class BallControllerScript : MonoBehaviour {
 		}
 	}
 
+    /// <summary>
+    /// Reads the data file for a level and creates two lists to represent the balls in the level.
+    /// Each line in the file should be in the format "l/0/0".
+    /// The first character in the line represents whether the ball spawns on the left or right side.
+    /// The second character represents what beat the ball spawns on.
+    /// The third character represents the color of the ball
+    /// The lines in the file should be in order from earliest beat to latest.
+    /// </summary>
 	void readBeatMap(){
 		StringReader stream = new StringReader (beatMap.text);
 		string line;
@@ -64,6 +76,11 @@ public class BallControllerScript : MonoBehaviour {
 		}
 	}
 
+    /// <summary>
+    /// Creates a ball GameObject and sets its color.
+    /// </summary>
+    /// <param name="side">The side the ball will spawn on.</param>
+    /// <param name="colorCode">A number representing the ball's color.</param>
 	void createBall(string side, double colorCode){
 		Vector2 position = side.Equals ("l") ? leftPosition : rightPosition;
 		currentBeat = Instantiate (ballPrefab, position, Quaternion.identity) as GameObject;
--- FronthandBackhand/Assets/Scripts/CameraShakeScript.cs
+++ FronthandBackhand/Assets/Scripts/CameraShakeScript.cs
@@ -1,52 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraShakeScript : MonoBehaviour {
-	
-	// How long the object should shake for.
-	public float shake = 0f;
-	
-	// Amplitude of the shake. A larger value shakes the camera harder.
-	public float shakeAmount = 0.7f;
-	public float decreaseFactor = 1.0f;
-	
-	public Vector3 originalPos;
+/// <summary>
+/// A script that creates camera shake. This should be attached to the camera object that will be shaking.
+/// If the camera needs to move around, put the camera in an empty game object and move that game object instead.
+/// </summary>
+public class CameraShakeScript : MonoBehaviour
+{
+    public static float DEFAULT_SHAKE_AMOUNT = .7f;
+    // How long the object should shake for.
+    public float shake = 0f;
 
-	private bool shakeStart;
-	
-	void Awake()
-	{
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
 
-	}
-	
-	void OnEnable()
-	{
-		originalPos =  Camera.main.transform.localPosition;
-	}
-	
-	void Update()
-	{
-		//if shakestart then set original and start shaking
-		if(shake>0 && !shakeStart)
-		{
-			shakeStart = true;
-		}
-		//if shake has started and still shake left
-		 else if (shake > 0 && shakeStart)
-		{
-			Camera.main.transform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
-			
-			shake -= Time.deltaTime * decreaseFactor;
-		}
-		else if(shake <= 0 && shakeStart)
-		{
-			shake = 0f;
-			Camera.main.transform.localPosition = originalPos;
-		}
-		else
-		{
-			Camera.main.transform.localPosition = originalPos;
-		}
-	}
+    public Vector3 originalPos;
+
+    private bool shakeStart;
+
+    /// <summary>
+    /// Sets the camera's original position, relative to its parent
+    /// </summary>
+    void OnEnable()
+    {
+        originalPos = Camera.main.transform.localPosition;
+    }
+
+    /// <summary>
+    /// Moves the camera to a random location within the radius of shakeAmount
+    /// </summary>
+    void Update()
+    {
+        //if shakestart then set original and start shaking
+        if (shake > 0 && !shakeStart)
+        {
+            shakeStart = true;
+        }
+        //if shake has started and still shake left
+        else if (shake > 0 && shakeStart)
+        {
+            Camera.main.transform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shake -= Time.deltaTime * decreaseFactor;
+        }
+        else if (shake <= 0 && shakeStart)
+        {
+            shake = 0f;
+            Camera.main.transform.localPosition = originalPos;
+        }
+        else
+        {
+            Camera.main.transform.localPosition = originalPos;
+        }
+    }
+
+    /// <summary>
+    /// Shakes the screen for the specified amount of time.
+    /// </summary>
+    /// <param name="shake">Time in seconds</param>
+    public void screenShake(float shake)
+    {
+        this.shake += shake;
+        shakeAmount = DEFAULT_SHAKE_AMOUNT;
+    }
+
+    /// <summary>
+    /// Shakes the screen for the specified amount of time at the specified amplitude.
+    /// </summary>
+    /// <param name="shakeAmount">Time in seconds</param>
+    /// <param name="shake">Amplitude of the shake</param>
+    public void screenShake(float shakeAmount, float shake)
+    {
+        this.shake += shake;
+        this.shakeAmount = shakeAmount;
+    }
 }
-
